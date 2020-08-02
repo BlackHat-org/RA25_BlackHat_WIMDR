73069b096de5ee5c6fd7e8ec83ccf1936e080cb4
harsh <harshverma85601@gmail.com>
2020-08-02 12:36:59 +0530

    add owner model

--- backendaas/api/views.py
+++ backendaas/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+
+from api.permissions import IsLoggedInUserOrAdmin, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer, ownerSerializer
+from .AAS import pred
+from . models import owner, User
+
+
+
+class ownerViewSet(viewsets.ModelViewSet):
+    queryset = owner.objects.all()
+    serializer_class = ownerSerializer
